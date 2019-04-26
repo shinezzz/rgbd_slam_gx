01244186a75c08ff797f3f8dec0bd9c5b5873dde
shinezzz <1879805956@qq.com>
2019-04-26 16:16:26 +0800

    [slamBase.h]add the third part's function

--- include/slamBase.h
+++ include/slamBase.h
@@ -21,6 +21,20 @@ struct CAMERA_INTRINSIC_PARAMETERS
 { 
     double cx, cy, fx, fy, scale;
 };
+// 帧结构
+struct FRAME
+{
+    cv::Mat rgb, depth; //该帧对应的彩色图与深度图
+    cv::Mat desp;       //特征描述子
+    vector<cv::KeyPoint> kp; //关键点
+};
+
+// PnP 结果
+struct RESULT_OF_PNP
+{
+    cv::Mat rvec, tvec;
+    int inliers;
+};
 
 // 函数接口
 // image2PonitCloud 将rgb图转换为点云
@@ -29,3 +43,57 @@ PointCloud::Ptr image2PointCloud( cv::Mat& rgb, cv::Mat& depth, CAMERA_INTRINSIC
 // point2dTo3d 将单个点从图像坐标转换为空间坐标
 // input: 3维点Point3f (u,v,d)
 cv::Point3f point2dTo3d( cv::Point3f& point, CAMERA_INTRINSIC_PARAMETERS& camera );
+
+// computeKeyPointsAndDesp 同时提取关键点与特征描述子
+void computeKeyPointsAndDesp( FRAME& frame, string detector, string descriptor );
+
+// estimateMotion 计算两个帧之间的运动
+// 输入：帧1和帧2, 相机内参
+RESULT_OF_PNP estimateMotion( FRAME& frame1, FRAME& frame2, CAMERA_INTRINSIC_PARAMETERS& camera );
+
+// 参数读取类
+class ParameterReader
+{
+public:
+    ParameterReader( string filename="./parameters.txt" )
+    {
+        ifstream fin( filename.c_str() );
+        if (!fin)
+        {
+            cerr<<"parameter file does not exist."<<endl;
+            return;
+        }
+        while(!fin.eof())
+        {
+            string str;
+            getline( fin, str );
+            if (str[0] == '#')
+            {
+                // 以‘＃’开头的是注释
+                continue;
+            }
+
+            int pos = str.find("=");
+            if (pos == -1)
+                continue;
+            string key = str.substr( 0, pos );
+            string value = str.substr( pos+1, str.length() );
+            data[key] = value;
+
+            if ( !fin.good() )
+                break;
+        }
+    }
+    string getData( string key )
+    {
+        map<string, string>::iterator iter = data.find(key);
+        if (iter == data.end())
+        {
+            cerr<<"Parameter name "<<key<<" not found!"<<endl;
+            return string("NOT_FOUND");
+        }
+        return iter->second;
+    }
+public:
+    map<string, string> data;
+};
