0c4d9b83edaaf9a625e0ed64cabfb5501d0d9ce6
shinezzz <1879805956@qq.com>
2019-04-26 16:19:38 +0800

    [slamBase.cpp]add third part

--- src/slamBase.cpp
+++ src/slamBase.cpp
@@ -43,3 +43,93 @@ cv::Point3f point2dTo3d( cv::Point3f& point, CAMERA_INTRINSIC_PARAMETERS& camera
     p.y = ( point.y - camera.cy) * p.z / camera.fy;
     return p;
 }
+// computeKeyPointsAndDesp 同时提取关键点与特征描述子
+void computeKeyPointsAndDesp( FRAME& frame, string detector, string descriptor )
+{
+    cv::Ptr<cv::FeatureDetector> _detector;
+    cv::Ptr<cv::DescriptorExtractor> _descriptor;
+
+    _detector = cv::FeatureDetector::create( detector.c_str() );
+    _descriptor = cv::DescriptorExtractor::create( descriptor.c_str() );
+
+    if (!_detector || !_descriptor)
+    {
+        cerr<<"Unknown detector or discriptor type !"<<detector<<","<<descriptor<<endl;
+        return;
+    }
+
+    _detector->detect( frame.rgb, frame.kp );
+    _descriptor->compute( frame.rgb, frame.kp, frame.desp );
+
+    return;
+}
+
+// estimateMotion 计算两个帧之间的运动
+// 输入：帧1和帧2
+// 输出：rvec 和 tvec
+RESULT_OF_PNP estimateMotion( FRAME& frame1, FRAME& frame2, CAMERA_INTRINSIC_PARAMETERS& camera )
+{
+    static ParameterReader pd;
+    vector< cv::DMatch > matches;
+    cv::BFMatcher matcher;
+    matcher.match( frame1.desp, frame2.desp, matches );
+   
+    cout<<"find total "<<matches.size()<<" matches."<<endl;
+    vector< cv::DMatch > goodMatches;
+    double minDis = 9999;
+    double good_match_threshold = atof( pd.getData( "good_match_threshold" ).c_str() );
+    for ( size_t i=0; i<matches.size(); i++ )
+    {
+        if ( matches[i].distance < minDis )
+            minDis = matches[i].distance;
+    }
+
+    for ( size_t i=0; i<matches.size(); i++ )
+    {
+        if (matches[i].distance < good_match_threshold*minDis)
+            goodMatches.push_back( matches[i] );
+    }
+
+    cout<<"good matches: "<<goodMatches.size()<<endl;
+    // 第一个帧的三维点
+    vector<cv::Point3f> pts_obj;
+    // 第二个帧的图像点
+    vector< cv::Point2f > pts_img;
+
+    // 相机内参
+    for (size_t i=0; i<goodMatches.size(); i++)
+    {
+        // query 是第一个, train 是第二个
+        cv::Point2f p = frame1.kp[goodMatches[i].queryIdx].pt;
+        // 获取d是要小心！x是向右的，y是向下的，所以y才是行，x是列！
+        ushort d = frame1.depth.ptr<ushort>( int(p.y) )[ int(p.x) ];
+        if (d == 0)
+            continue;
+        pts_img.push_back( cv::Point2f( frame2.kp[goodMatches[i].trainIdx].pt ) );
+
+        // 将(u,v,d)转成(x,y,z)
+        cv::Point3f pt ( p.x, p.y, d );
+        cv::Point3f pd = point2dTo3d( pt, camera );
+        pts_obj.push_back( pd );
+    }
+
+    double camera_matrix_data[3][3] = {
+        {camera.fx, 0, camera.cx},
+        {0, camera.fy, camera.cy},
+        {0, 0, 1}
+    };
+
+    cout<<"solving pnp"<<endl;
+    // 构建相机矩阵
+    cv::Mat cameraMatrix( 3, 3, CV_64F, camera_matrix_data );
+    cv::Mat rvec, tvec, inliers;
+    // 求解pnp
+    cv::solvePnPRansac( pts_obj, pts_img, cameraMatrix, cv::Mat(), rvec, tvec, false, 100, 1.0, 100, inliers );
+
+    RESULT_OF_PNP result;
+    result.rvec = rvec;
+    result.tvec = tvec;
+    result.inliers = inliers.rows;
+
+    return result;
+}
