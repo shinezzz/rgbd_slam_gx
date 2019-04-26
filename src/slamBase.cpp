d7c21134b9442c2a6201724f803f2df54af95910
shinezzz <1879805956@qq.com>
2019-04-26 22:26:10 +0800

    [slamBase.cpp]add the fifth part of slamBase.cpp

--- src/slamBase.cpp
+++ src/slamBase.cpp
@@ -1,10 +1,19 @@
+/*************************************************************************
+	> File Name: src/slamBase.cpp
+	> Author: xiang gao
+	> Mail: gaoxiang12@mails.tsinghua.edu.cn
+    > Implementation of slamBase.h
+	> Created Time: 2015年07月18日 星期六 15时31分49秒
+ ************************************************************************/
+
 #include "slamBase.h"
 
-PointCloud::Ptr image2PointCloud( cv::Mat& rgb, cv::Mat& depth,CAMERA_INTRINSIC_PARAMETERS& camera ){
+PointCloud::Ptr image2PointCloud( cv::Mat& rgb, cv::Mat& depth, CAMERA_INTRINSIC_PARAMETERS& camera )
+{
     PointCloud::Ptr cloud ( new PointCloud );
 
-    for (int m = 0; m < depth.rows; m++)
-        for (int n=0; n < depth.cols; n++)
+    for (int m = 0; m < depth.rows; m+=2)
+        for (int n=0; n < depth.cols; n+=2)
         {
             // 获取深度图中(m,n)处的值
             ushort d = depth.ptr<ushort>(m)[n];
@@ -35,6 +44,7 @@ PointCloud::Ptr image2PointCloud( cv::Mat& rgb, cv::Mat& depth,CAMERA_INTRINSIC_
 
     return cloud;
 }
+
 cv::Point3f point2dTo3d( cv::Point3f& point, CAMERA_INTRINSIC_PARAMETERS& camera )
 {
     cv::Point3f p; // 3D 点
@@ -43,6 +53,7 @@ cv::Point3f point2dTo3d( cv::Point3f& point, CAMERA_INTRINSIC_PARAMETERS& camera
     p.y = ( point.y - camera.cy) * p.z / camera.fy;
     return p;
 }
+
 // computeKeyPointsAndDesp 同时提取关键点与特征描述子
 void computeKeyPointsAndDesp( FRAME& frame, string detector, string descriptor )
 {
@@ -74,7 +85,7 @@ RESULT_OF_PNP estimateMotion( FRAME& frame1, FRAME& frame2, CAMERA_INTRINSIC_PAR
     cv::BFMatcher matcher;
     matcher.match( frame1.desp, frame2.desp, matches );
    
-    cout<<"find total "<<matches.size()<<" matches."<<endl;
+    RESULT_OF_PNP result;
     vector< cv::DMatch > goodMatches;
     double minDis = 9999;
     double good_match_threshold = atof( pd.getData( "good_match_threshold" ).c_str() );
@@ -83,14 +94,24 @@ RESULT_OF_PNP estimateMotion( FRAME& frame1, FRAME& frame2, CAMERA_INTRINSIC_PAR
         if ( matches[i].distance < minDis )
             minDis = matches[i].distance;
     }
+    
+    cout<<"min dis = "<<minDis<<endl;
+    if ( minDis < 10 ) 
+        minDis = 10;
 
     for ( size_t i=0; i<matches.size(); i++ )
     {
-        if (matches[i].distance < good_match_threshold*minDis)
+        if (matches[i].distance < good_match_threshold*minDis )
             goodMatches.push_back( matches[i] );
     }
 
     cout<<"good matches: "<<goodMatches.size()<<endl;
+
+    if (goodMatches.size() <= 5) 
+    {
+        result.inliers = -1;
+        return result;
+    }
     // 第一个帧的三维点
     vector<cv::Point3f> pts_obj;
     // 第二个帧的图像点
@@ -113,23 +134,72 @@ RESULT_OF_PNP estimateMotion( FRAME& frame1, FRAME& frame2, CAMERA_INTRINSIC_PAR
         pts_obj.push_back( pd );
     }
 
+    if (pts_obj.size() ==0 || pts_img.size()==0)
+    {
+        result.inliers = -1;
+        return result;
+    }
+
     double camera_matrix_data[3][3] = {
         {camera.fx, 0, camera.cx},
         {0, camera.fy, camera.cy},
         {0, 0, 1}
     };
 
-    cout<<"solving pnp"<<endl;
     // 构建相机矩阵
     cv::Mat cameraMatrix( 3, 3, CV_64F, camera_matrix_data );
     cv::Mat rvec, tvec, inliers;
     // 求解pnp
     cv::solvePnPRansac( pts_obj, pts_img, cameraMatrix, cv::Mat(), rvec, tvec, false, 100, 1.0, 100, inliers );
 
-    RESULT_OF_PNP result;
     result.rvec = rvec;
     result.tvec = tvec;
     result.inliers = inliers.rows;
 
     return result;
+}
+
+
+// cvMat2Eigen
+Eigen::Isometry3d cvMat2Eigen( cv::Mat& rvec, cv::Mat& tvec )
+{
+    cv::Mat R;
+    cv::Rodrigues( rvec, R );
+    Eigen::Matrix3d r;
+    for ( int i=0; i<3; i++ )
+        for ( int j=0; j<3; j++ ) 
+            r(i,j) = R.at<double>(i,j);
+  
+    // 将平移向量和旋转矩阵转换成变换矩阵
+    Eigen::Isometry3d T = Eigen::Isometry3d::Identity();
+
+    Eigen::AngleAxisd angle(r);
+    T = angle;
+    T(0,3) = tvec.at<double>(0,0); 
+    T(1,3) = tvec.at<double>(1,0); 
+    T(2,3) = tvec.at<double>(2,0);
+    return T;
+}
+
+// joinPointCloud 
+// 输入：原始点云，新来的帧以及它的位姿
+// 输出：将新来帧加到原始帧后的图像
+PointCloud::Ptr joinPointCloud( PointCloud::Ptr original, FRAME& newFrame, Eigen::Isometry3d T, CAMERA_INTRINSIC_PARAMETERS& camera ) 
+{
+    PointCloud::Ptr newCloud = image2PointCloud( newFrame.rgb, newFrame.depth, camera );
+
+    // 合并点云
+    PointCloud::Ptr output (new PointCloud());
+    pcl::transformPointCloud( *original, *output, T.matrix() );
+    *newCloud += *output;
+
+    // Voxel grid 滤波降采样
+    static pcl::VoxelGrid<PointT> voxel;
+    static ParameterReader pd;
+    double gridsize = atof( pd.getData("voxel_grid").c_str() );
+    voxel.setLeafSize( gridsize, gridsize, gridsize );
+    voxel.setInputCloud( newCloud );
+    PointCloud::Ptr tmp( new PointCloud() );
+    voxel.filter( *tmp );
+    return tmp;
 }
