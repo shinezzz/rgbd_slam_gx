ecb0a700b76d55cfac96adc1684014a66b78f605
shinezzz <1879805956@qq.com>
2019-04-28 20:30:46 +0800

    part VII

--- include/slamBase.h
+++ include/slamBase.h
@@ -22,6 +22,7 @@ using namespace std;
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/eigen.hpp>
 
 
 // PCL
@@ -44,6 +45,7 @@ struct CAMERA_INTRINSIC_PARAMETERS
 // 帧结构
 struct FRAME
 {
+    int frameID; 
     cv::Mat rgb, depth; //该帧对应的彩色图与深度图
     cv::Mat desp;       //特征描述子
     vector<cv::KeyPoint> kp; //关键点
@@ -135,3 +137,24 @@ inline static CAMERA_INTRINSIC_PARAMETERS getDefaultCamera()
     camera.scale = atof( pd.getData( "camera.scale" ).c_str() );
     return camera;
 }
+
+
+
+//the following are UBUNTU/LINUX ONLY terminal color
+#define RESET "\033[0m"
+#define BLACK "\033[30m" /* Black */
+#define RED "\033[31m" /* Red */
+#define GREEN "\033[32m" /* Green */
+#define YELLOW "\033[33m" /* Yellow */
+#define BLUE "\033[34m" /* Blue */
+#define MAGENTA "\033[35m" /* Magenta */
+#define CYAN "\033[36m" /* Cyan */
+#define WHITE "\033[37m" /* White */
+#define BOLDBLACK "\033[1m\033[30m" /* Bold Black */
+#define BOLDRED "\033[1m\033[31m" /* Bold Red */
+#define BOLDGREEN "\033[1m\033[32m" /* Bold Green */
+#define BOLDYELLOW "\033[1m\033[33m" /* Bold Yellow */
+#define BOLDBLUE "\033[1m\033[34m" /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
+#define BOLDCYAN "\033[1m\033[36m" /* Bold Cyan */
+#define BOLDWHITE "\033[1m\033[37m" /* Bold White */
